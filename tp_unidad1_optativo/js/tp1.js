7edb5292e95a2a7235d63e6e9f590c62d9205fac
Juan Pablo Cesarini <jcesarini@matelab.com.ar>
2021-08-31 21:25:24 -0300

    Texto mejorado en div precio

--- tp_unidad1_optativo/js/tp1.js
+++ tp_unidad1_optativo/js/tp1.js
@@ -2,13 +2,13 @@ function mostrarPrecio() {
     const opcion = document.getElementById("seguro").value;
     switch (opcion) {
         case "Basico":
-            document.getElementById("precio").innerHTML = "$ 100";
+            document.getElementById("precio").innerHTML = "El precio del seguro es $ 100";
             break;
         case "Intermedio":
-            document.getElementById("precio").innerHTML = "$ 1000";
+            document.getElementById("precio").innerHTML = "El precio del seguro es $ 1000";
             break;
         case "Premium":
-            document.getElementById("precio").innerHTML = "$ 1500";
+            document.getElementById("precio").innerHTML = "El precio del seguro es $ 1500";
             break;
         default:
             document.getElementById("precio").innerHTML = "";
