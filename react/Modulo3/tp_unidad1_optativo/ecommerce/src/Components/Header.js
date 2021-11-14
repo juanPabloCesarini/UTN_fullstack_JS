0f9d6939245041c8f812c1fd8e37e7b951de10c0
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-11-14 16:53:20 -0300

    correccion de header

--- react/Modulo3/tp_unidad1_optativo/ecommerce/src/Components/Header.js
+++ react/Modulo3/tp_unidad1_optativo/ecommerce/src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <>
         <div className="container-fluid bg-success p-3">
             <h1 className="text-center">UTN - Curso fullstack JS</h1>
-            <h3 className="text-center">TP OBLIGATORIO MOD 2</h3>
+            <h3 className="text-center">TP 1 MOD 3</h3>
         </div>
         <Menu/>
         </>
